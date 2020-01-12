19eb39b661c5190353be5fe6f0197b5df03fa1ed
Luciano Mariño <jmarino@falabella.com.ar>
2020-01-12 02:25:16 -0300

    .

--- src/controllers/MatchesController.php
+++ src/controllers/MatchesController.php
@@ -10,7 +10,7 @@ class MatchesController extends Controller
 	{	
 		$args = [
 			"navbar" => [
-				"username_session" 	=> $_SESSION["user_session"]->username,
+				"username_session" 	=> $_SESSION["user_session"]->display_name,
 			],
 		];
 		
